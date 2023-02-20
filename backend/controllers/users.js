26f6e52802b72283fc2414634da76d2c7a5a11b0
MariaMantusova <maria.mantusova@mail.ru>
2023-02-20 17:31:44 +0300

    fix: fix errors in user

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -15,7 +15,13 @@ const getUsers = (req, res, next) => User.find({})
 const getUserById = (req, res, next) => User.findById(req.params.userId)
   .orFail(() => new NotFoundError('Пользователь не найден'))
   .then((user) => res.status(200).send({ data: user }))
-  .catch(next);
+  .catch((err) => {
+    if (err.name === 'CastError') {
+      next(new ValidationError());
+    } else {
+      next(err);
+    }
+  });
 
 const getCurrentUser = (req, res, next) => User.findById(req.user._id)
   .then((user) => res.status(200).send({ data: user }))
@@ -40,8 +46,10 @@ const createUser = ((req, res, next) => {
     .catch((err) => {
       if (err.code === 11000) {
         next(new ExistingEmailError());
-      } else {
+      } else if (err.name === 'CastError') {
         next(new ValidationError());
+      } else {
+        next(err);
       }
     });
 });
@@ -49,27 +57,31 @@ const createUser = ((req, res, next) => {
 function changeInfo(req, res, next) {
   const { name, about } = req.body;
 
-  User.findById(req.user._id)
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .orFail(() => new NotFoundError('Пользователь не найден'))
-    .then(() => {
-      User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
-        .then((user) => res.status(200).send({ data: user }))
-        .catch(() => next(new ValidationError()));
-    })
-    .catch(next);
+    .then((user) => res.status(200).send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new ValidationError());
+      } else {
+        next(err);
+      }
+    });
 }
 
 const changeAvatar = (req, res, next) => {
   const { avatar } = req.body;
 
-  User.findById(req.user._id)
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .orFail(() => new NotFoundError('Пользователь не найден'))
-    .then(() => {
-      User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
-        .then((user) => res.status(200).send({ data: user }))
-        .catch(() => next(new ValidationError()));
-    })
-    .catch(next);
+    .then((user) => res.status(200).send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new ValidationError());
+      } else {
+        next(err);
+      }
+    });
 };
 
 const login = (req, res, next) => {
