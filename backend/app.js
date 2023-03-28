447e3c980d45e3f6b5c4e33e7968811e6de98b50
MariaMantusova <maria.mantusova@mail.ru>
2023-03-28 18:05:49 +0300

    feaT: allow all

--- backend/app.js
+++ backend/app.js
@@ -41,8 +41,6 @@ app.use('/', (req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  res.header('Access-Control-Allow-Origin', "*");
-
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', 'true');
